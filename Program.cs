96127e605a3854204e0e559510f29cee73101cc2
KirillOrig <dpouldark@mail.ru>
2023-12-25 13:41:52 +0300

    1 задача 2 пример через строковый вид

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@
 
 int num = new Random().Next(100, 1000);
 
-int num1 = num / 100;
-int num2 = num % 10;
+string num1 = Convert.ToString(num / 100);
+string num2 = Convert.ToString(num % 10);
 
 Console.WriteLine(num);
-Console.WriteLine(num1 + "" + num2);
-Console.WriteLine(num1 * 10 + num2);
+Console.WriteLine(num1 + num2);
+//Console.WriteLine(num1 * 10 + num2);
 
-Console.WriteLine($"{num} => {num1 + "" + num2}");
+//Console.WriteLine($"{num} => {num1 + "" + num2}");
 
 
 
