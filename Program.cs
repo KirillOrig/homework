71b1c99630ed82bf4b1549f8f8287a1723dcfc0e
KirillOrig <dpouldark@mail.ru>
2023-12-31 19:00:02 +0300

    2 задача ДЗ

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
-// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
-// 7 и 23.
 
+// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и
+// выдаёт номер координатной четверти плоскости, в которой находится эта точка.
 
+Console.Write($"Введите первую координату точки. num1 = ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите вторую координату точки. num2 = ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число N = ");
-int N = Convert.ToInt32(Console.ReadLine());
-
-if (N % 7 == 0 && N % 23 == 0)
+if (num1 != 0 && num2 != 0)
 {
-    Console.WriteLine($"Число '{N}' одновременно кратно '7' и '23'.");
+    if (num1 < 0 && num2 < 0)
+    {
+        Console.WriteLine($"Точка находится в III четверти");
+    }
+    if (num1 > 0 && num2 > 0)
+    {
+        Console.WriteLine($"Точка находится в I четверти");
+    }
+    if (num1 < 0 && num2 > 0)
+    {
+        Console.WriteLine($"Точка находится в II четверти");
+    }
+    if (num1 > 0 && num2 < 0)
+    {
+        Console.WriteLine($"Точка находится в IV четверти");
+    }
 }
-else
-{
-    Console.WriteLine($"Число '{N}' одновременно не кратно числам '7' и '23'.");
-}
-
 
 
